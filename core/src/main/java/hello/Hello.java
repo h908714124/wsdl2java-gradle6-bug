c4c0b772ef29c980aabf1230eff4ca1fdf7af0e4
h908714124 <kraftdurchblumen@gmx.de>
2020-01-03 20:26:26 +0100

    fix 4

--- core/src/main/java/hello/Hello.java
+++ core/src/main/java/hello/Hello.java
@@ -2,8 +2,12 @@ package hello;
 
 import com.foo.foo.zoink.Data;
 
+import baiu.Allah;
+
 public class Hello {
 
 
-
+    public static void main(String[] args) {
+        System.out.println(new Data());
+    }
 }
---
+++ generator/src/main/java/baiu/Allah.java
@@ -0,0 +1,10 @@
+package baiu;
+
+import com.foo.foo.zoink.Data;
+
+public class Allah {
+
+    public Data getData() {
+        return null;
+    }
+}
