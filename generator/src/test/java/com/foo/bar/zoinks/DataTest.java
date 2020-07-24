3569fc8276df5660c3253c2e762e02709cba2b2c
h908714124 <kraftdurchblumen@gmx.de>
2020-07-24 11:11:23 +0200

    update

--- generator/src/test/java/com/foo/bar/zoinks/DataTest.java
+++ generator/src/test/java/com/foo/bar/zoinks/DataTest.java
@@ -1,9 +1,13 @@
-package com.foo.bar.zoink;
+package com.foo.bar.zoinks;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.bind.*;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -21,18 +25,21 @@ class DataTest {
         jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
     }
 
+    /**
+     * Demonstrates data loss during JAXB marshal.
+     */
     @Test
-    void remarshalTest() throws DatatypeConfigurationException, JAXBException {
+    void marshalTest() throws DatatypeConfigurationException, JAXBException {
         Data data1 = new Data();
         XMLGregorianCalendar cal = DatatypeFactory.newInstance().newXMLGregorianCalendar();
         cal.setDay(20);
         cal.setMonth(11);
-        cal.setYear(2010);
+        cal.setYear(2010); // not setting hours, minutes, seconds
         data1.setCreated(cal);
-        System.out.println("created 1: " + data1.getCreated());
+        System.out.println("created 1: " + data1.getCreated()); // 2010-11-20
         String xml = marshal(data1);
         Data data2 = unmarshal(xml);
-        System.out.println("created 2: " + data2.getCreated());
+        System.out.println("created 2: " + data2.getCreated()); // null
     }
 
     private String marshal(Data data) throws JAXBException {
